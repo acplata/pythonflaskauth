47332a70f78407cb4219e65a09032ec9db8f8cd7
Ana Plata <acplata@Anas-MacBook-Air.local>
2023-09-06 18:14:34 -0400

    initial

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -4,10 +4,9 @@ import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 
 import { Home } from "./pages/home";
-import { Demo } from "./pages/demo";
-import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
-
+import Login from "./pages/Login.jsx";
+import Private from "./pages/Private.jsx";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
@@ -17,7 +16,8 @@ const Layout = () => {
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
 
-    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
+    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") 
+    return <BackendURL/ >;
 
     return (
         <div>
@@ -26,8 +26,8 @@ const Layout = () => {
                     <Navbar />
                     <Routes>
                         <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
+                        <Route element={<Login />} path="/login" />
+                        <Route element={<Private />} path="/private" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
---
+++ src/front/js/pages/Login.jsx
@@ -0,0 +1,49 @@
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+const initialValue = {
+  email: "",
+  password: "",
+};
+
+const Login = () => {
+  const { store, actions } = useContext(Context);
+  const [registerData, setRegisterData] = useState(initialValue);
+  const navigate = useNavigate();
+  const handleChange = (event) => {
+    
+    setRegisterData({
+      ...registerData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async () => {
+    const result = await actions.loginUser(registerData);
+    if (result) return navigate("/private");
+    alert("Inicio invalido");
+  };
+  return (
+    <div className="text-center mt-5">
+      <div className="container">Inicia sesion</div>
+      <div className="card p-5">
+        <input
+          placeholder="email"
+          type="email"
+          name="email"
+          onChange={handleChange}
+        />
+        <input
+          placeholder="password"
+          type="password"
+          name="password"
+          onChange={handleChange}
+        />
+        <button onClick={handleSubmit}>Iniciar sesion</button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
